7ac5ff7e5091a75d8400c9747ec8bf3248bb6d63
Nikal Morgan <nikalmorgan11@gmail.com>
2020-08-25 16:49:07 -0500

    Correcting Signup

--- homepage/forms.py
+++ homepage/forms.py
@@ -4,11 +4,12 @@ from customUsers.models import MyUser
 
 
 class SignupForm(forms.ModelForm):
+    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = MyUser
-        fields = '__all__'
+        fields = ['firstname', 'lastname', 'age', 'displayname', 'street', 'apt', 'city', 'state']
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=240)
